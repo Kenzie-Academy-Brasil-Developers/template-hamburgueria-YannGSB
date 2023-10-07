b34de60795f5ccd19ddd6943e3f27485289a4a6f
Yann G. Simonetti Buffet <yanngsb@hotmail.com>
2023-10-07 16:35:06 -0300

    fix: identation and organization of the code

--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -1,11 +1,11 @@
 import { ProductCard } from "./ProductCard";
 
 export const ProductList = ({ productList }) => {
-   return (
-      <ul>
-         {productList.map((product) => (
-            <ProductCard key={product.id} product={product} />
-         ))}
-      </ul>
-   );
+  return (
+    <ul>
+      {productList.map((product) => (
+        <ProductCard key={product.id} product={product} />
+      ))}
+    </ul>
+  );
 };
